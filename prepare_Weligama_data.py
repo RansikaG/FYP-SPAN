a209ad39344b99ffdcbe165b639969f094af063c
RansikaG <ransika1998@gmail.com>
2023-03-09 11:06:25 +0530

    gallery_query_train_val

--- prepare_Weligama_data.py
+++ prepare_Weligama_data.py
@@ -1,5 +1,6 @@
 import os
 import random
+import shutil
 
 data_path = '/home/fyp3-2/Desktop/BATCH18/Weligama_data'
 image_folder_path = os.path.join(data_path, 'images')
@@ -22,6 +23,28 @@ index_range = int(len(new_boat_names) * train_split)
 train_boats = new_boat_names[index_range + 1:]
 test_boats = new_boat_names[:index_range]
 
-os.mkdir('/home/fyp3-2/Desktop/BATCH18/Weligama_data/reid/train')
-for boat in new_boat_names:
+os.mkdir(data_path + '/reid/train')
+os.mkdir(data_path + '/reid/valid')
+for boat in train_boats:
+    path = os.path.join(data_path, boat)
+    for (root, dirs, files) in os.walk(path, topdown=True):
+        images = files
+        random_img = random.choice(images)
+        valid_image_path = data_path + '/reid/valid/' + boat
+        os.mkdir(valid_image_path)
+        shutil.move(os.path.join(root, random_img), os.path.join(valid_image_path, random_img))
+        train_image_path = data_path + '/reid/train'
+        shutil.move(os.path.join(root, boat), os.path.join(train_image_path, boat))
 
+os.mkdir(data_path + '/reid/gallery')
+os.mkdir(data_path + '/reid/query')
+for boat in test_boats:
+    path = os.path.join(image_folder_path, boat)
+    for (root, dirs, files) in os.walk(path, topdown=True):
+        images = files
+        random_img = random.choice(images)
+        query_image_path = data_path + '/reid/query/' + boat
+        os.mkdir(query_image_path)
+        shutil.move(os.path.join(root, random_img), os.path.join(query_image_path, random_img))
+        gallery_image_path = data_path + '/reid/gallery'
+        shutil.move(os.path.join(root, boat), os.path.join(gallery_image_path, boat))
