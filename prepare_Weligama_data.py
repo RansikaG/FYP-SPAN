77f776717cecdea9b82061976ecca7c438b26273
--unset-all <ransika1988@gmail.com>
2023-03-09 11:52:20 +0530

    fix

--- prepare_Weligama_data.py
+++ prepare_Weligama_data.py
@@ -3,7 +3,6 @@ import random
 import shutil
 
 data_path = '/home/fyp3-2/Desktop/BATCH18/Weligama_data'
-image_folder_path = os.path.join(data_path, 'images')
 
 boat_folders = []
 
@@ -17,12 +16,13 @@ for i, boat in enumerate(boat_folders):
     os.rename(os.path.join(data_path, boat), os.path.join(data_path, num))
     new_boat_names.append(num)
 
-train_split = 0.2
+train_split = 0.8
 random.shuffle(new_boat_names)
 index_range = int(len(new_boat_names) * train_split)
-train_boats = new_boat_names[index_range + 1:]
-test_boats = new_boat_names[:index_range]
+train_boats = new_boat_names[:index_range]
+test_boats = new_boat_names[index_range:]
 
+os.mkdir(data_path + '/reid')
 os.mkdir(data_path + '/reid/train')
 os.mkdir(data_path + '/reid/valid')
 for boat in train_boats:
@@ -34,12 +34,12 @@ for boat in train_boats:
         os.mkdir(valid_image_path)
         shutil.move(os.path.join(root, random_img), os.path.join(valid_image_path, random_img))
         train_image_path = data_path + '/reid/train'
-        shutil.move(os.path.join(root, boat), os.path.join(train_image_path, boat))
+        shutil.move(root, os.path.join(train_image_path, boat))
 
 os.mkdir(data_path + '/reid/gallery')
 os.mkdir(data_path + '/reid/query')
 for boat in test_boats:
-    path = os.path.join(image_folder_path, boat)
+    path = os.path.join(data_path, boat)
     for (root, dirs, files) in os.walk(path, topdown=True):
         images = files
         random_img = random.choice(images)
@@ -47,4 +47,4 @@ for boat in test_boats:
         os.mkdir(query_image_path)
         shutil.move(os.path.join(root, random_img), os.path.join(query_image_path, random_img))
         gallery_image_path = data_path + '/reid/gallery'
-        shutil.move(os.path.join(root, boat), os.path.join(gallery_image_path, boat))
+        shutil.move(root, os.path.join(gallery_image_path, boat))
